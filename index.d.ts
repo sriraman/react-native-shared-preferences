5cb34114c042159b825e315958ac19ec86bb5fd7
Dopey2 <daniel@tootsweet.app>
2019-01-30 11:51:41 +0100

    add type definition

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+type GetItemCallback = (value: string | null) => void;
+type GetMultipleItemsCallback = (values: string[] | null) => void;
+
+declare module "react-native-shared-preferences" {
+    const setItem: (key: string , value: string) => void;
+    const getItem: (key: string, callback: GetItemCallback) => void;
+    const getMultipleItems: (keys: string[] , calback: GetMultipleItemsCallback) => void;
+    const getAll: (calback: GetMultipleItemsCallback) => void;
+    const clear: () => void;
+    const getAllKeys: (callback: GetMultipleItemsCallback) => void;
+    const removeItem: (key: string) => void;
+}
